95646d120645b8a5db8f8f909c4002b5a3840b15
Mariella Page <mariella@mariellapage.com>
2021-09-25 14:13:33 -0600

    Added Parrots to Reg_UserJs

--- Marified/Regular/user-overrides.js
+++ Marified/Regular/user-overrides.js
@@ -1,3 +1,6 @@
+user_pref("_mar.js.parrot", "MAR OVERRIDES STARTED");
+
+user_pref("_mar.searching.js.parrot", "MAR OVERRIDES Searching STARTED");
 /* 0801: disable location bar using search
  * Don't leak URL typos to a search engine, give an error message instead
  * Examples: "secretplace,com", "secretplace/com", "secretplace com", "secret place.com"
@@ -16,7 +19,30 @@ user_pref("keyword.enabled", true);
 user_pref("browser.search.suggest.enabled", true);
 user_pref("browser.urlbar.suggest.searches", true);
 
+/* 5010: disable location bar suggestion types
+ * [SETTING] Privacy & Security>Address Bar>When using the address bar, suggest ***/
+// MAR UNCOMMENTED the line below
+user_pref("browser.urlbar.suggest.history", false);
+// MAR UNCOMMENTED the line below
+   user_pref("browser.urlbar.suggest.bookmark", false);
+// MAR UNCOMMENTED the line below
+   user_pref("browser.urlbar.suggest.openpage", false);
+// MAR UNCOMMENTED the line below
+   user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
 
+/* 5011: disable location bar dropdown
+ * This value controls the total number of entries to appear in the location bar dropdown ***/
+   // user_pref("browser.urlbar.maxRichResults", 0);
+
+/* 5012: disable location bar autofill
+ * [1] https://support.mozilla.org/kb/address-bar-autocomplete-firefox#w_url-autocomplete ***/
+// MAR UNCOMMENTED the line below
+   user_pref("browser.urlbar.autoFill", false);
+
+user_pref("_mar.searching.js.parrot", "MAR OVERRIDES Searching FINISHED");
+
+
+user_pref("_mar.OptSec.js.parrot", "MAR OVERRIDES OptSec STARTED");
 /* 0810: disable coloring of visited links
  * [SETUP-HARDEN] Bulk rapid history sniffing was mitigated in 2010 [1][2]. Slower and more expensive
  * redraw timing attacks were largely mitigated in FF77+ [3]. Using RFP (4501) further hampers timing
@@ -57,25 +83,6 @@ user_pref("privacy.window.maxInnerHeight", 900);
 // MAR UNCOMMENTED the line below
    user_pref("browser.sessionstore.resume_from_crash", false);
 
-/* 5010: disable location bar suggestion types
- * [SETTING] Privacy & Security>Address Bar>When using the address bar, suggest ***/
-// MAR UNCOMMENTED the line below
-   user_pref("browser.urlbar.suggest.history", false);
-// MAR UNCOMMENTED the line below
-   user_pref("browser.urlbar.suggest.bookmark", false);
-// MAR UNCOMMENTED the line below
-   user_pref("browser.urlbar.suggest.openpage", false);
-// MAR UNCOMMENTED the line below
-   user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
-
-/* 5011: disable location bar dropdown
- * This value controls the total number of entries to appear in the location bar dropdown ***/
-   // user_pref("browser.urlbar.maxRichResults", 0);
-
-/* 5012: disable location bar autofill
- * [1] https://support.mozilla.org/kb/address-bar-autocomplete-firefox#w_url-autocomplete ***/
-   // user_pref("browser.urlbar.autoFill", false);
-
 /* 5013: disable browsing and download history
  * [NOTE] We also clear history and downloads on exit (2803)
  * [SETTING] Privacy & Security>History>Custom Settings>Remember browsing and download history ***/
@@ -87,6 +94,9 @@ user_pref("browser.download.autohideButton", false); // [FF57+]
 // MAR UNCOMMENTED the line below AND changed FROM 2 TO 0
 user_pref("layout.spellcheckDefault", 0); // 0=none, 1-multi-line, 2=multi-line & single-line
 
+user_pref("_mar.OptSec.js.parrot", "MAR OVERRIDES OptSec FINISHED");
+
+user_pref("_mar.Ux.js.parrot", "MAR OVERRIDES Ux STARTED");
 /* UX FEATURES ***/
 user_pref("browser.messaging-system.whatsNewPanel.enabled", false); // What's New toolbar icon [FF69+]
 // MAR UNCOMMENTED the line below
@@ -96,4 +106,5 @@ user_pref("browser.messaging-system.whatsNewPanel.enabled", false); // What's Ne
    user_pref("identity.fxaccounts.enabled", false); // Firefox Accounts & Sync [FF60+] [RESTART]
 // MAR UNCOMMENTED the line below AND changed FROM false TO true
    user_pref("reader.parse-on-load.enabled", true); // Reader View
-
+user_pref("_mar.Ux.js.parrot", "MAR OVERRIDES Ux FINISHED");
+user_pref("_mar.js.parrot", "MAR OVERRIDES STARTED");
